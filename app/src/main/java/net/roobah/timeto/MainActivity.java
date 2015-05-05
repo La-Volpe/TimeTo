ab826069c7a7699412199abe9200f2065e7d0d84
La-Volpe <javadarjmandi@gmail.com>
2015-05-05 04:53:25 +0430

    Cleaning code. The Main Activity has 4 counters now, added an Icon, fixed some bugs

--- app/src/main/java/net/roobah/timeto/MainActivity.java
+++ app/src/main/java/net/roobah/timeto/MainActivity.java
@@ -2,10 +2,7 @@ package net.roobah.timeto;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.os.CountDownTimer;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -23,10 +20,11 @@ public class MainActivity extends Activity {
         //View:
         final ProgressBar prec1 = (ProgressBar) findViewById(R.id.prec1);
         prec1.setProgress(100);
-        final Button start1 = (Button)  findViewById(R.id.start1);
+
         final TextView counter1 = (TextView) findViewById(R.id.counter1);
+        counter1.setText("زمان تا شروع");
         //Time goodies:
-        final Date destination1 = new Date(1433907000000L);
+        final Date destination1 = new Date(1434079800000L);
         final Date theTimeForNow = new Date();
 
 
@@ -45,58 +43,158 @@ public class MainActivity extends Activity {
                 long remainingTime = (long) (millisUntilFinished - theTimeForNow.getTime());
                 Date remainingTimeInMillis = new Date(remainingTime);
                 String message =
-                        "Remaining Time to Konkoor: " + remainingTimeInMillis.getDate() +
-                        " days and "+ remainingTimeInMillis.getHours() +" Hours and "+
-                        remainingTimeInMillis.getMinutes() + " Minutes and " +
-                        remainingTimeInMillis.getSeconds() + " Seconds";
+                        "زمان تا کنکور: " + remainingTimeInMillis.getDate() +
+                        " روز و "+ remainingTimeInMillis.getHours() +" ساعت و "+
+                        remainingTimeInMillis.getMinutes() + " دقیقه و  " +
+                        remainingTimeInMillis.getSeconds() + " ثانیه";
                 counter1.setText(message);
             }
 
             @Override
             public void onFinish() {
-                counter1.setText("Finished");
+                counter1.setText("پایان یافت");
                 prec1.setProgress(0);
 
             }
         };
 
 
+        //Second Counter:
+
+        final ProgressBar prec2 = (ProgressBar) findViewById(R.id.prec2);
+        prec2.setProgress(100);
+
+        final TextView counter2 = (TextView) findViewById(R.id.counter2);
+        counter2.setText("زمان تا شروع");
+        //Time goodies:
+        final Date destination2 = new Date(1434079800000L);
 
 
 
-        start1.setOnClickListener(new View.OnClickListener() {
+
+        //Decleration of the first counter:
+        final CountDownTimer timer2 = new CountDownTimer(destination2.getTime(),1000) {
             @Override
-            public void onClick(View view) {
-                Vibrator vibe =
-                        (Vibrator) getSystemService(getApplicationContext().VIBRATOR_SERVICE);
-                vibe.vibrate(50);
-                timer1.start();
+            public void onTick(long millisUntilFinished) {
+                int precent = (int) (millisUntilFinished  /(double)destination2.getTime() * 100);
+                prec2.setProgress(precent);
+
+
+                //Text to show:
+
+                // The Actual Time remaining:
+                long remainingTime = (long) (millisUntilFinished - theTimeForNow.getTime());
+                Date remainingTimeInMillis = new Date(remainingTime);
+                String message =
+                        "زمان تا کنکور: " + remainingTimeInMillis.getDate() +
+                                " روز و "+ remainingTimeInMillis.getHours() +" ساعت و "+
+                                remainingTimeInMillis.getMinutes() + " دقیقه و  " +
+                                remainingTimeInMillis.getSeconds() + " ثانیه";
+                counter2.setText(message);
             }
-        });
+
+            @Override
+            public void onFinish() {
+                counter2.setText("پایان یافت");
+                prec2.setProgress(0);
+
+            }
+        };
+
+        //Third Counter:
+
+        final ProgressBar prec3 = (ProgressBar) findViewById(R.id.prec3);
+        prec3.setProgress(100);
+
+        final TextView counter3 = (TextView) findViewById(R.id.counter3);
+        counter3.setText("زمان تا شروع");
+        //Time goodies:
+        final Date destination3 = new Date(1434079800000L);
+
+
+
+
+        //Decleration of the first counter:
+        final CountDownTimer timer3 = new CountDownTimer(destination3.getTime(),1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int precent = (int) (millisUntilFinished  /(double)destination3.getTime() * 100);
+                prec3.setProgress(precent);
+
+
+                //Text to show:
+
+                // The Actual Time remaining:
+                long remainingTime = (long) (millisUntilFinished - theTimeForNow.getTime());
+                Date remainingTimeInMillis = new Date(remainingTime);
+                String message =
+                        "زمان تا کنکور: " + remainingTimeInMillis.getDate() +
+                                " روز و "+ remainingTimeInMillis.getHours() +" ساعت و "+
+                                remainingTimeInMillis.getMinutes() + " دقیقه و  " +
+                                remainingTimeInMillis.getSeconds() + " ثانیه";
+                counter3.setText(message);
+            }
+
+            @Override
+            public void onFinish() {
+                counter3.setText("پایان یافت");
+                prec3.setProgress(0);
+
+            }
+        };
+
+        //Forth Timer:
+
+        final ProgressBar prec4 = (ProgressBar) findViewById(R.id.prec4);
+        prec4.setProgress(100);
+
+        final TextView counter4 = (TextView) findViewById(R.id.counter4);
+        counter1.setText("زمان تا شروع");
+        //Time goodies:
+        final Date destination4 = new Date(1434079800000L);
+
+
+
+
+        //Decleration of the first counter:
+        final CountDownTimer timer4 = new CountDownTimer(destination4.getTime(),1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int precent = (int) (millisUntilFinished  /(double)destination4.getTime() * 100);
+                prec4.setProgress(precent);
+
+
+                //Text to show:
+
+                // The Actual Time remaining:
+                long remainingTime = (long) (millisUntilFinished - theTimeForNow.getTime());
+                Date remainingTimeInMillis = new Date(remainingTime);
+                String message =
+                        "زمان تا کنکور: " + remainingTimeInMillis.getDate() +
+                                " روز و "+ remainingTimeInMillis.getHours() +" ساعت و "+
+                                remainingTimeInMillis.getMinutes() + " دقیقه و  " +
+                                remainingTimeInMillis.getSeconds() + " ثانیه";
+                counter4.setText(message);
+            }
+
+            @Override
+            public void onFinish() {
+                counter4.setText("پایان یافت");
+                prec4.setProgress(0);
+
+            }
+        };
+
+
+
+        timer1.start();
+        timer2.start();
+        timer3.start();
+        timer4.start();
+
 
 
     }
 
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
